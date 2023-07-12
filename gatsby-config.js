db36f0c690ed3b95473c2d21dc70d5602120dbb5
Eric Russell <cire1776@gmail.com>
2023-07-11 21:38:01 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My first Gatsby v5 site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
